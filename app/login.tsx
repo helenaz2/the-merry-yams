d2bb77f546549642869cca0ce615f294702d092a
Marryam <marryamz6308@gmail.com>
2026-03-10 20:04:20 -0700

    extra

--- app/login.tsx
+++ app/login.tsx
@@ -13,7 +13,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        padding: 20, // Adds breathing room on the sides
+        padding: 20,
         backgroundColor: "#fff",
     },
     title: {
