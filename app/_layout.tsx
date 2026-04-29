2b87ca70a394cd2f134b45e2cb1d4709e382d0cc
Marryam <marryamz6308@gmail.com>
2026-04-29 10:43:53 -0700

    small change

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,6 +6,10 @@ export default function RootLayout() {
       <Stack.Screen name="index" options={{ headerTitle: "Landing" }} />
       <Stack.Screen name="login" options={{ headerTitle: "Login" }} />
       <Stack.Screen name="signup" options={{ headerTitle: "Sign Up" }} />
+      <Stack.Screen name="all-work-spaces" />
+      <Stack.Screen name="individual-workspace" />
+      <Stack.Screen name="create-workspace" />
+      <Stack.Screen name="scanner-page" />
     </Stack>
   );
 }
